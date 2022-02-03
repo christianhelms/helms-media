056de571ba84ec655bbe86a8e0439e634c9232e3
christianhelms <helmsmedia@gmail.com>
2022-02-02 21:20:44 -0600

    changed site logo

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Transition from '../utils/Transition';
 import Dropdown from '../utils/Dropdown';
+import Logo from '../images/helms-media-logo.svg';
 
 function Header() {
 
@@ -49,17 +50,8 @@ function Header() {
           {/* Site branding */}
           <div className="shrink-0 mr-4">
             {/* Logo */}
-            <Link to="/" className="block" aria-label="Cruip">
-              <svg className="w-8 h-8" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-                <defs>
-                  <radialGradient cx="21.152%" cy="86.063%" fx="21.152%" fy="86.063%" r="79.941%" id="header-logo">
-                    <stop stopColor="#4FD1C5" offset="0%" />
-                    <stop stopColor="#81E6D9" offset="25.871%" />
-                    <stop stopColor="#338CF5" offset="100%" />
-                  </radialGradient>
-                </defs>
-                <rect width="32" height="32" rx="16" fill="url(#header-logo)" fillRule="nonzero" />
-              </svg>
+            <Link to="/" className="block" aria-label="Helms Media">
+            <img src={Logo} width="32" height="32" rx="16" />
             </Link>
           </div>
 
