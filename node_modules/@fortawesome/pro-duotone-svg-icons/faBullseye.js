69ec030869718ea3f9bde72e03381fd5f6f99f04
christianhelms <helmsmedia@gmail.com>
2022-02-03 14:13:42 -0600

    FA Pro added

---
+++ node_modules/@fortawesome/pro-duotone-svg-icons/faBullseye.js
@@ -0,0 +1,29 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+var prefix = 'fad';
+var iconName = 'bullseye';
+var width = 496;
+var height = 512;
+var ligatures = [];
+var unicode = 'f140';
+var svgPathData = ['M248,440A184,184,0,1,1,432,256,183.89818,183.89818,0,0,1,248,440Zm0-312A128,128,0,1,0,376,256,128.00313,128.00313,0,0,0,248,128Zm0,192a64,64,0,1,1,64-64A64.07211,64.07211,0,0,1,248,320Z', 'M248,8C111.03,8,0,119.03,0,256S111.03,504,248,504,496,392.97,496,256,384.97,8,248,8Zm0,432A184,184,0,1,1,432,256,183.89818,183.89818,0,0,1,248,440Zm0-312A128,128,0,1,0,376,256,128.00313,128.00313,0,0,0,248,128Zm0,192a64,64,0,1,1,64-64A64.07211,64.07211,0,0,1,248,320Z'];
+
+exports.definition = {
+  prefix: prefix,
+  iconName: iconName,
+  icon: [
+    width,
+    height,
+    ligatures,
+    unicode,
+    svgPathData
+  ]};
+
+exports.faBullseye = exports.definition;
+exports.prefix = prefix;
+exports.iconName = iconName;
+exports.width = width;
+exports.height = height;
+exports.ligatures = ligatures;
+exports.unicode = unicode;
+exports.svgPathData = svgPathData;
