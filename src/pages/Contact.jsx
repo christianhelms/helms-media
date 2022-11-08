b311a0dd0d20cbc42fcd377539ab0074dd5d81de
christianhelms <helmsmedia@gmail.com>
2022-11-07 21:00:07 -0600

    contact form updates

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 import Header from '../partials/Header';
 
 import Footer from '../partials/Footer';
 
 function Contact() {
-
+  const { register, handleSubmit, errors, reset } = useForm();
+  function onSubmitForm(values) {
+    console.log(values);
+  }
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
 
@@ -27,27 +31,27 @@ function Contact() {
               </div>
 
               {/* Contact form */}
-              <form className="max-w-xl mx-auto">
+              <form onSubmit={handleSubmit(onSubmitForm)} className="max-w-xl mx-auto">
                 <div className="flex flex-wrap -mx-3 mb-4">
                   <div className="w-full md:w-1/2 px-3 mb-4 md:mb-0">
                     <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="first-name">First Name <span className="text-red-600">*</span></label>
-                    <input id="first-name" type="text" className="form-input w-full text-gray-800" placeholder="Enter your first name" required />
+                    <input id="first-name" type="text" href={register} className="form-input w-full text-gray-800" placeholder="Enter your first name" required />
                   </div>
                   <div className="w-full md:w-1/2 px-3">
                     <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="last-name">Last Name <span className="text-red-600">*</span></label>
-                    <input id="last-name" type="text" className="form-input w-full text-gray-800" placeholder="Enter your last name" required />
+                    <input id="last-name" type="text" href={register} className="form-input w-full text-gray-800" placeholder="Enter your last name" required />
                   </div>
                 </div>
                 <div className="flex flex-wrap -mx-3 mb-4">
                   <div className="w-full px-3">
                     <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="email">Email <span className="text-red-600">*</span></label>
-                    <input id="email" type="email" className="form-input w-full text-gray-800" placeholder="Enter your email address" required />
+                    <input id="email" type="email" href={register} className="form-input w-full text-gray-800" placeholder="Enter your email address" required />
                   </div>
                 </div>
                 <div className="flex flex-wrap -mx-3 mb-4">
                   <div className="w-full px-3">
                     <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="subject">Subject <span className="text-red-600">*</span></label>
-                    <input id="subject" type="text" className="form-input w-full text-gray-800" placeholder="How can we help you?" required />
+                    <input id="subject" type="text" href={register} className="form-input w-full text-gray-800" placeholder="How can we help you?" required />
                   </div>
                 </div>
                 <div className="flex flex-wrap -mx-3 mb-4">
@@ -63,7 +67,7 @@ function Contact() {
                 <div className="flex flex-wrap -mx-3 mb-4">
                   <div className="w-full px-3">
                     <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="message">Message</label>
-                    <textarea id="message" rows="4" className="form-textarea w-full text-gray-800" placeholder="Write your message"></textarea>
+                    <textarea id="message" rows="4" href={register} className="form-textarea w-full text-gray-800" placeholder="Write your message"></textarea>
                   </div>
                 </div>
                 <div className="flex flex-wrap -mx-3 mt-4">
